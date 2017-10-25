c52dee345a8459c452cf803b55e60acaf1340d35
mudoY <61540975@qq.com>
2017-10-25 15:46:31 +0800

    feat:finish 4、 5、 6、 7、

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
@@ -2,28 +2,59 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice6DrawLineView extends View {
 
+    private Paint mPaint;
+    private int mWidth;
+    private int mHeight;
+
     public Practice6DrawLineView(Context context) {
         super(context);
+        init();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(10f);
+        mPaint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        练习内容：使用 canvas.drawLine() 方法画直线
+        //        练习内容：使用 canvas.drawLine() 方法画直线
+        canvas.translate(mWidth/2,mHeight/2);
+
+        canvas.drawLine(-200,-100,200,100,mPaint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
@@ -2,28 +2,59 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice7DrawRoundRectView extends View {
 
+    private Paint mPaint;
+    private int mWidth;
+    private int mHeight;
+
     public Practice7DrawRoundRectView(Context context) {
         super(context);
+        init();
     }
 
     public Practice7DrawRoundRectView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice7DrawRoundRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(10f);
+        mPaint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+        //        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+        canvas.translate(mWidth/2,mHeight/2);
+
+        canvas.drawRoundRect(-200,-100,200,100,40,40,mPaint);
     }
 }
