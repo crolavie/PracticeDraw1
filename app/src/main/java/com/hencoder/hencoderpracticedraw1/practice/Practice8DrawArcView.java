e02a61797462e1e9f40de1ce3b1e458bb135d91d
mudoY <61540975@qq.com>
2017-10-25 16:08:40 +0800

    feat:finish 8/9

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,28 +2,67 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice8DrawArcView extends View {
 
+    private Paint mPaint;
+    private int mWidth;
+    private int mHeight;
+
     public Practice8DrawArcView(Context context) {
         super(context);
+        init();
     }
 
     public Practice8DrawArcView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice8DrawArcView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(2f);
+        mPaint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        canvas.translate(mWidth / 2, mHeight / 2);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(-200, -125, 200, 125, -100, 100, true, mPaint);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(-200, -125, 200, 125, 20, 140, false, mPaint);
+
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(-200, -125, 200, 125, 180, 60, false, mPaint);
+
     }
 }
