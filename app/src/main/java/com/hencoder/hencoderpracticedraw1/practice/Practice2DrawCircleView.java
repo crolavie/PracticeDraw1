f1b86ae41693e10953305d54d22b99e00a6dc490
mudoY <61540975@qq.com>
2017-10-25 15:29:39 +0800

    feat:finish practice 2 and 3

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,29 +2,80 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
 
+    private int mWidth;
+    private int mHeight;
+    private Paint mPaint;
+
     public Practice2DrawCircleView(Context context) {
         super(context);
+        init();
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5f);
+        mPaint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        练习内容：使用 canvas.drawCircle() 方法画圆
-//        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        //        练习内容：使用 canvas.drawCircle() 方法画圆
+        //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        canvas.translate(mWidth / 2, mHeight / 2);
+
+        // 实心圆
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(-200, -200, 150, mPaint);
+
+        // 空心圆
+        mPaint.setStrokeWidth(5f);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(200, -200, 150, mPaint);
+
+        // 蓝色实心圆
+        mPaint.setColor(Color.parseColor("#4A90E2"));
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(-200, 200, 150, mPaint);
+
+        // 线宽为20的空心圆
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(40f);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(200, 200, 150, mPaint);
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,22 +2,50 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice3DrawRectView extends View {
 
+    private Paint mPaint;
+    private int mWidth;
+    private int mHeight;
+
     public Practice3DrawRectView(Context context) {
         super(context);
+        init();
     }
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(5f);
+        mPaint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 
     @Override
@@ -25,5 +53,7 @@ public class Practice3DrawRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRect() 方法画矩形
+        canvas.translate(mWidth/2,mHeight/2);
+        canvas.drawRect(-200,-200,200,200,mPaint);
     }
 }
